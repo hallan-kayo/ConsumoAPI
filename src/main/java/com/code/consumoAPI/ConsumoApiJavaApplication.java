18c07e7c7b3b7ff306702006c9ff57596b4902ec
Halan Caio <hallankayo20@gmail.com>
2023-10-13 12:28:04 -0300

    First Commit

---
+++ src/main/java/com/code/consumoAPI/ConsumoApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.code.consumoAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumoApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumoApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/consumoAPI/ConsumoApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.consumoAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumoApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
